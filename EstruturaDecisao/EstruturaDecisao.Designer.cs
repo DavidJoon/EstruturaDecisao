4058901d1067424a43808334aecfeb7ad93de0bb
David Jonathan <david.jonn1@hotmail.com>
2021-10-12 15:28:14 -0300

    Adicionar arquivos de projeto.

---
+++ EstruturaDecisao/EstruturaDecisao.Designer.cs
@@ -0,0 +1,178 @@
+﻿
+namespace EstruturaDecisao
+{
+    partial class EstruturaDecisao
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelTitulo = new System.Windows.Forms.Label();
+            this.labelNum01 = new System.Windows.Forms.Label();
+            this.labelNum02 = new System.Windows.Forms.Label();
+            this.labelNum03 = new System.Windows.Forms.Label();
+            this.labelResultado = new System.Windows.Forms.Label();
+            this.buttonCalcular = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelTitulo
+            // 
+            this.labelTitulo.AutoSize = true;
+            this.labelTitulo.BackColor = System.Drawing.Color.FloralWhite;
+            this.labelTitulo.Font = new System.Drawing.Font("Constantia", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelTitulo.Location = new System.Drawing.Point(50, 18);
+            this.labelTitulo.Name = "labelTitulo";
+            this.labelTitulo.Size = new System.Drawing.Size(200, 19);
+            this.labelTitulo.TabIndex = 0;
+            this.labelTitulo.Text = "Qual é o Maior Número ?";
+            // 
+            // labelNum01
+            // 
+            this.labelNum01.BackColor = System.Drawing.Color.FloralWhite;
+            this.labelNum01.Font = new System.Drawing.Font("Ebrima", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelNum01.Location = new System.Drawing.Point(51, 61);
+            this.labelNum01.Name = "labelNum01";
+            this.labelNum01.Size = new System.Drawing.Size(73, 23);
+            this.labelNum01.TabIndex = 1;
+            this.labelNum01.Text = "Número 1:";
+            this.labelNum01.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelNum02
+            // 
+            this.labelNum02.BackColor = System.Drawing.Color.FloralWhite;
+            this.labelNum02.Font = new System.Drawing.Font("Ebrima", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelNum02.Location = new System.Drawing.Point(51, 93);
+            this.labelNum02.Name = "labelNum02";
+            this.labelNum02.Size = new System.Drawing.Size(73, 23);
+            this.labelNum02.TabIndex = 2;
+            this.labelNum02.Text = "Número 2:";
+            this.labelNum02.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelNum03
+            // 
+            this.labelNum03.BackColor = System.Drawing.Color.FloralWhite;
+            this.labelNum03.Font = new System.Drawing.Font("Ebrima", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelNum03.Location = new System.Drawing.Point(51, 125);
+            this.labelNum03.Name = "labelNum03";
+            this.labelNum03.Size = new System.Drawing.Size(73, 23);
+            this.labelNum03.TabIndex = 3;
+            this.labelNum03.Text = "Número 3:";
+            this.labelNum03.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.BackColor = System.Drawing.Color.FloralWhite;
+            this.labelResultado.Location = new System.Drawing.Point(50, 195);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(200, 43);
+            this.labelResultado.TabIndex = 4;
+            this.labelResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonCalcular
+            // 
+            this.buttonCalcular.BackColor = System.Drawing.Color.LimeGreen;
+            this.buttonCalcular.Font = new System.Drawing.Font("Ebrima", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.buttonCalcular.Location = new System.Drawing.Point(112, 154);
+            this.buttonCalcular.Name = "buttonCalcular";
+            this.buttonCalcular.Size = new System.Drawing.Size(75, 28);
+            this.buttonCalcular.TabIndex = 5;
+            this.buttonCalcular.Text = "Descobrir";
+            this.buttonCalcular.UseVisualStyleBackColor = false;
+            this.buttonCalcular.Click += new System.EventHandler(this.buttonCalcular_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numericUpDown1.Location = new System.Drawing.Point(130, 61);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 9;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numericUpDown2.Location = new System.Drawing.Point(130, 93);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown2.TabIndex = 10;
+            this.numericUpDown2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.numericUpDown3.Location = new System.Drawing.Point(130, 125);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown3.TabIndex = 11;
+            this.numericUpDown3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // EstruturaDecisao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gold;
+            this.ClientSize = new System.Drawing.Size(290, 247);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.buttonCalcular);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.labelNum03);
+            this.Controls.Add(this.labelNum02);
+            this.Controls.Add(this.labelNum01);
+            this.Controls.Add(this.labelTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "EstruturaDecisao";
+            this.ShowIcon = false;
+            this.Text = "Estrutura de Decisão";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelTitulo;
+        private System.Windows.Forms.Label labelNum01;
+        private System.Windows.Forms.Label labelNum02;
+        private System.Windows.Forms.Label labelNum03;
+        private System.Windows.Forms.Label labelResultado;
+        private System.Windows.Forms.Button buttonCalcular;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ EstruturaDecisao/EstruturaDecisao.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EstruturaDecisao
+{
+    public partial class EstruturaDecisao : Form
+    {
+        public EstruturaDecisao()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalcular_Click(object sender, EventArgs e)
+        {
+            int num01 = Convert.ToInt32(numericUpDown1.Value);
+            int num02 = Convert.ToInt32(numericUpDown2.Value);
+            int num03 = Convert.ToInt32(numericUpDown3.Value);
+            int maior;
+
+            if (num01 == num02 && num02 == num03) {
+                labelResultado.Text = "Valores são iguais.";
+                return;
+            }
+
+            if (num01 == num03 && num01 > num02 && num03 > num02)
+            {
+                labelResultado.Text = "Valores " + num01 + " e " + num03 + " são maiores.";
+                return;
+            }
+
+            if (num02 == num03 && num02 > num01 && num03 > num01)
+            {
+                labelResultado.Text = "Valores " + num02 + " e " + num03 + " são maiores.";
+                return;
+            }
+
+            if (num01 == num02 && num01 > num03 && num02 > num03)
+            {
+                labelResultado.Text = "Valores " + num01 + " e " + num02 + " são maiores.";
+                return;
+            }
+
+            if (num01 > num02 && num01 > num03)
+            {
+                maior = num01;
+
+            }else if(num02 > num01 && num02 > num03)
+            {
+                maior = num02;
+            }else{
+                maior = num03;
+            }
+            labelResultado.Text = "O número " + maior + " é o maior.";
+        }
+    }
+}
